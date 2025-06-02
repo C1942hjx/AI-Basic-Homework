e3b3a11a53ad5e463bc9538561435a0fa82c62fa
C1942hjx <3113088952@qq.com>
2025-06-02 15:01:58 +0800

    增加了 Amazon 图书数据的 RAG (#9)
    * 更改了goodreads.py 的内容
    
    * 添加了 Amazon 书籍信息的 RAG

--- goodreads.py
+++ goodreads.py
@@ -1,11 +1,11 @@
 from camel.embeddings import SentenceTransformerEncoder
 from camel.retrievers import VectorRetriever
-from model_base import Qwen_72B_Instruct,Deepseek_V3
+from model_base import Deepseek_R1,Deepseek_V3
 from camel.agents import ChatAgent
 
 embedding_model=SentenceTransformerEncoder(model_name='e5-small-v2')
 
-model=Qwen_72B_Instruct()
+model=Deepseek_R1()
 model2=Deepseek_V3()
 
 # 创建并初始化一个向量数据库 (以QdrantStorage为例)
@@ -22,13 +22,21 @@ vr = VectorRetriever(embedding_model= embedding_model,storage=vector_storage)
 # 图书元数据： 书名、作者（完整署名）、ISBN、出版社   物理属性：页码数、语言版本  出版信息：精确到日的出版日期
 # 包含评分，评分人数信息
 
+def Storagequery(query,top_k=5):
+    
+    results = vr.query(query=query, top_k=top_k)
+
+    results_string=str(results)
+
+    return results_string
+
 def GoodreadsSearch(query,top_k=5):
 
     #提取关键信息
 
     print("正在准备...")
 
-    msg1="你是一个书籍推荐助手，给出用户的问题，请提取出关键信息并整理，接下来需要在 Goodreads 向量数据集中进行检索，请用相应国家的语言和英语分别输出，同时不要输出多余的内容干扰向量信息检索"
+    msg1="你是一个书籍推荐助手，给出用户的问题，请提取出关键信息并整理，接下来需要在 Goodreads 向量数据集中进行检索，请用**书籍或作者所在国家的语言**和英语分别输出，同时**不要输出除关键词以外多余的内容干扰向量信息检索**"
     agent1=ChatAgent(model=model,system_message=msg1)
     response1=agent1.step(query)
     res=response1.msgs[0].content
@@ -37,9 +45,7 @@ def GoodreadsSearch(query,top_k=5):
 
     print("正在进行搜索...")
 
-    results = vr.query(query=res, top_k=top_k)
-
-    results_string=str(results)
+    results_string=Storagequery(res,top_k=top_k)
 
     #整理搜索结果
 
