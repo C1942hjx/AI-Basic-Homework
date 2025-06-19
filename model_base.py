347737cccd5a15506b7eb691d6d507d134d5809f
C1942hjx <3113088952@qq.com>
2025-06-19 20:24:56 +0800

    现在调用模型可以设置模型参数，包括maxtoken，temperature，top—p (#13)

--- model_base.py
+++ model_base.py
@@ -12,49 +12,49 @@ qwen_url=os.getenv("QWEN_URL")
 deepseek_api_key = os.getenv("DEEPSEEK_API_KEY")
 deepseek_url=os.getenv("DEEPSEEK_URL")
 
-def Deepseek_R1 ():
+def Deepseek_R1 (max_tokens=16384,temperature=0.6,top_p=0.95):
     return ModelFactory.create(
         model_platform=ModelPlatformType.OPENAI_COMPATIBLE_MODEL,
         model_type="deepseek-ai/DeepSeek-R1",
         url=deepseek_url,
         api_key=deepseek_api_key,
-        model_config_dict={"max_tokens":16000}
+        model_config_dict={"max_tokens":max_tokens,"temperature":temperature,"top_p":top_p}
     )
 
-def Deepseek_V3 ():
+def Deepseek_V3 (max_tokens=8192,temperature=0.7,top_p=0.7):
     return ModelFactory.create(
         model_platform=ModelPlatformType.OPENAI_COMPATIBLE_MODEL,
         model_type="deepseek-ai/DeepSeek-V3",
         url=deepseek_url,
         api_key=deepseek_api_key,
-        model_config_dict={"max_tokens":8000}
+        model_config_dict={"max_tokens":max_tokens,"temperature":temperature,"top_p":top_p}
     )
 
-def Qwen_72B_Instruct ():
+def Qwen_72B_Instruct (max_tokens=4096,temperature=0.7,top_p=0.7):
     return ModelFactory.create(
         model_platform=ModelPlatformType.OPENAI_COMPATIBLE_MODEL,
         model_type="Qwen/Qwen2.5-72B-Instruct",
         url=qwen_url,
         api_key=qwen_api_key,
-        model_config_dict={"max_tokens":4000}
+        model_config_dict={"max_tokens":max_tokens,"temperature":temperature,"top_p":top_p}
     )
 
 
-def Qwen_VL_72B_Instruct ():
+def Qwen_VL_72B_Instruct (max_tokens=4096,temperature=0.7,top_p=0.7):
     return ModelFactory.create(
         model_platform=ModelPlatformType.OPENAI_COMPATIBLE_MODEL,
         model_type="Qwen/Qwen2.5-VL-72B-Instruct",
         url=qwen_url,
         api_key=qwen_api_key,
-        model_config_dict={"max_tokens":4000}
+        model_config_dict={"max_tokens":max_tokens,"temperature":temperature,"top_p":top_p}
     )
 
 
-def Qwen3_8B ():
+def Qwen3_8B (max_tokens=8192,temperature=0.6,top_p=0.95):
     return ModelFactory.create(
         model_platform=ModelPlatformType.OPENAI_COMPATIBLE_MODEL,
         model_type="Qwen/Qwen3-8B",
         url=qwen_url,
         api_key=qwen_api_key,
-        model_config_dict={"max_tokens":8000}
+        model_config_dict={"max_tokens":max_tokens,"temperature":temperature,"top_p":top_p}
     )
