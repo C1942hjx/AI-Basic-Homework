032f7b2efb4c4789aeded6891ef268a503eb7c83
zaozao_zmx <49331792+zhangmixuan@users.noreply.github.com>
2025-06-22 23:41:18 +0800

    敏感词过滤 (#20)

--- text_query.py
+++ text_query.py
@@ -9,17 +9,24 @@ model1 = Deepseek_V3()
 def Text_query(user_question):
     print("正在进行搜索...")
     
-    search_1 = Google_search(user_question,5)
+    search_1 = Google_search(user_question,4)
     
-    print("正在整理搜索结果...")
+    system_msg_3 = "你是一个敏感词屏蔽器，告诉你用户文段和网络搜索结果，你需要判断这些内容是否是无意义内容或属于敏感内容，如果不是无意义内容也不属于中国意义下的敏感或反动，返回数字\"1\"，否则返回数字\"0\"，注意回答中除了数字1或0不能含有任何冗余信息。"
+    chat_agent_3 = ChatAgent(model=model1,system_message=system_msg_3,output_language='zh')
+    question_3 = "用户文段：" +user_question + "\n搜索结果：" + search_1
+    response_3 = chat_agent_3.step(question_3)
+    content_3=response_3.msgs[0].content
+    if content_3=="0":
+        print("这段文字是无意义内容或存在敏感信息，暂时无法回答。")
+        return ""
 
+    print("正在整理搜索结果...")
     system_msg_1 = "你是一个图书检索助手，你需要回答给定的文段来自哪本书籍，保证了文段一定来自某本具体的书的原文，这点很重要，你可以根据给出的章节信息和搜索结果给出最有可能的10本书并分别给出理由。"
 
     chat_agent_1 = ChatAgent(model=model1,system_message=system_msg_1,output_language='zh')
     question_1 = "用户给的文段：" + user_question + "   网络搜索结果：" + search_1
     response_1 = chat_agent_1.step(question_1)
     content_1=response_1.msgs[0].content
-
 
     system_msg_2 = "你是一个审查员，图书检索助手给出了10本和用户给的文段最符合的书并给出了理由，你的目标是按照文段是否原样出现在书籍中将这几本书排序，你需要按照推荐顺序给出这5本书，你不需要给出排除的理由，你只需要给出这5本书和对这5本书推荐的理由，别的任何消息都不要输出。"
 
